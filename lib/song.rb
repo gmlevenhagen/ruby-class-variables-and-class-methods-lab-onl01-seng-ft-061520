e93d5f9740ab75bc6422a64ef1c6e705f206d20a
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 12:53:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,15 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = []
-  @@genres = []
+  @@artist = []
+  @@genre = []
 
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artists
+    @@artist << artist
     @@genre << genre
   end
 
