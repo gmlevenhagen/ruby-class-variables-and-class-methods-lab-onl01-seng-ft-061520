4c457e7cb6be5464e980bd64208592c105ee5724
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 12:50:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,19 @@ class Song
   def self.count
     @@count
   end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genre.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
 end
