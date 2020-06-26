6617fc4e869b7b95291bed4f4765b668917a64a9
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 13:05:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
+    genre_count = []
     @@genre.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
@@ -40,7 +40,7 @@ class Song
   end
 
   def self.artist_count
-    artist_count = {}
+    artist_count = []
       @@artists.each do |artist|
         if artist_count[artist]
         artist_count[artist] +=1
